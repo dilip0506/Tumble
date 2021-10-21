1cee7339964be772bf2e2744de1bdaa71159bdd8
Dilip Nasu <ndilip0506@gmail.com>
2021-10-20 21:25:03 -0500

    renaming and change configs

--- Tumble.Api/Startup.cs
+++ Tumble.Api/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Tumble.Api.Extensions;
 using Tumble.Services;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -42,6 +35,7 @@ namespace Tumble.Api
 
             services.Configure<DataAccess.Settings>(Configuration.GetSection("DatabaseConnections"));
             services.Configure<Services.Email.EmailSettings>(Configuration.GetSection("EmailSettings"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
